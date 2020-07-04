a77a22523d1c81fc5ebc0379df185593c9020181
Vsevolod Kompantsev <59050128+diditforlulz273@users.noreply.github.com>
2020-07-04 16:03:06 +0300

    Update Vanilla.py

--- PokerRL/game/_/wrappers/Vanilla.py
+++ PokerRL/game/_/wrappers/Vanilla.py
@@ -25,7 +25,7 @@ class VanillaWrapper(Wrapper):
         self.env.print_obs(wrapped_obs)
 
     def get_current_obs(self, env_obs=None):
-        if env_obs in not None:
+        if env_obs is not None:
             return env_obs
         return self.env.get_current_obs()
 
